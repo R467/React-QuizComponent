a16b7145849498dbc7ecffa9ef9b793f86e45fcd
Rob <robert.bell@rxpservices.com>
2021-11-16 19:59:00 +1100

    Error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,12 +7,25 @@ class QuizQuestion extends Component {
         super(props)
 
         this.props = props
+
+        this.state = {
+            incorrectAnswer: false
+        }
     }
 
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
+            this.setState((state) => {
+                return { incorrectAnswer : false }
+            })
         }
+        else {
+            this.setState((state) => {
+                return { incorrectAnswer : true }
+            })
+        }
+        
     }
 
     render() { 
@@ -27,6 +40,7 @@ class QuizQuestion extends Component {
                 ))}
             </ul>
             </section>
+            {this.state.incorrectAnswer === true ? <p className='error'>Sorry, that's not right.</p> : null} 
         </main>
     }
 
